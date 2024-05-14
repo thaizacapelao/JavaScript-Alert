a12cd782216e215a5f8300a4b4107765c2377235
Thaiza Capelão <thaizacapelao@gmail.com>
2024-05-14 16:50:34 -0400

    adicionando condicionais e comparacao

--- Alert-JavaScript/app.js
+++ Alert-JavaScript/app.js
@@ -1,3 +1,14 @@
 alert('Bem vindo ao jogo Secret Number!');
+
 let numeroSecreto = 29;
-prompt('Digite um número entre 1 e 30');
+
+let chute = prompt('Digite um número entre 1 e 30');
+
+if (numeroSecreto == chute) {
+    console.log('Você descobriu o Secret Number (29)');
+} else {
+    console.log('Você errou o número');
+};
+
+
+// == Usamamos dois == para comparações;
